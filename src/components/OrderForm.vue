d47e295b0f98ab92ffd81732c6455559ccf1e0fd
chizi56 <bauka1215@gmail.com>
2022-05-29 02:08:33 +0600

    fix: select items

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -2,7 +2,7 @@
     <div>
         <table>
             <tr>
-                <td v-for="item of items" :key="item.id" @click="select(item)" v-bind:class="{selected: item.selected}">
+                <td  v-for="item of items" :key="item.id"   @click="select(item)" v-bind:class="{selected: item.selected}">
                     <img v-if="item.status !== 'ripped'" :src="item.img" />
                     
                 </td>  
@@ -14,8 +14,10 @@
                 </td>
             </tr>
         </table>
-        <h2>Всего: {{this.total}}кг</h2>
+        
         <form v-if="this.total != 0">
+            <h2>Всего: {{this.total}}кг</h2>
+            <h3>Кол-во арбузов: {{this.count}}</h3>
             <input placeholder="Введите адрес доставки: " type="text"/>
             <input placeholder="Введите свой номер телефона: " type="number" />
             <input type="datetime-local" v-bind:min="this.dateTime" v-bind:max="this.requiredTime" step=1800 />
@@ -37,15 +39,22 @@
                 count: 0,
                 dateTime: '',
                 requiredTime: '',
+                selected: [],
                 items: [
                     {id:1,img: "/images/ripe.png", status: 'ripe', selected: false, weight: this.weight()},
                     {id:2,img: "/images/ripe.png", status: 'ripe', selected: false, weight: this.weight()},
                     {id:3,img: "/images/unripe.png", status: 'unripe', selected: false, weight: this.weight()},
+                    {id:4,img: "/images/ripe.png", status: 'ripped', selected: false, weight: this.weight()},
+                    {id:5,img: "/images/ripe.png", status: 'ripe', selected: false, weight: this.weight()},
+                    {id:6,img: "/images/unripe.png", status: 'unripe', selected: false, weight: this.weight()}
                 ],
                 items2: [
                     {id:1,img: "#", status: 'ripped', selected: false, weight: this.weight()},
                     {id:2,img: "/images/ripe.png", status: 'ripe', selected: false, weight: this.weight()},
                     {id:3,img: "/images/unripe.png", status: 'unripe', selected: false, weight: this.weight()},
+                    {id:4,img: "/images/ripe.png", status: 'ripe', selected: false, weight: this.weight()},
+                    {id:5,img: "/images/unripe.png", status: 'unripe', selected: false, weight: this.weight()},
+                    {id:6,img: "/images/ripe.png", status: 'ripe', selected: false, weight: this.weight()}
                 ]
             }
         },
@@ -53,6 +62,8 @@
             var currentdate = new Date(); 
             var month = 0;
             var day = 0;
+            var hour = 0;
+            var minutes = 0;
             const monthConverter = (currentMonth) => {
                 if(currentMonth >= 10){
                  return currentMonth;
@@ -71,18 +82,36 @@
                     return day;
                 }
             }
+            const hoursConverter = (currentHour) => {
+                if(currentHour >= 10){
+                 return currentHour;
+                }else{
+                    hour.toString();
+                    hour = "0" + (currentHour);
+                    return hour;
+                }
+            }
+            const minutesConverter = (currentMinutes) => {
+                if(currentMinutes >= 10){
+                 return currentMinutes;
+                }else{
+                    minutes.toString();
+                    minutes = "0" + (currentMinutes);
+                    return minutes;
+                }
+            }
             this.dateTime = currentdate.getFullYear() + "-"
              + monthConverter(currentdate.getMonth()+1) + "-"
               + dayConverter(currentdate.getDate()) + "T"
-               + currentdate.getHours() + ":"  
-                + currentdate.getMinutes();
+               + hoursConverter(currentdate.getHours()) + ":"  
+                + minutesConverter(currentdate.getMinutes());
             var myDate = new Date(new Date().getTime()+(9*24*60*60*1000));
             
             this.requiredTime = myDate.getFullYear() + "-"
              + monthConverter(myDate.getMonth()+1) + "-"
               + dayConverter(myDate.getDate()) + "T"
-               + myDate.getHours() + ":"  
-                + myDate.getMinutes();
+               + hoursConverter(myDate.getHours()) + ":"  
+                + minutesConverter(myDate.getMinutes());
             console.log("Date before: ", this.dateTime);
             console.log("Date after: ", this.requiredTime);
         },
@@ -91,20 +120,37 @@
                 return Math.floor((Math.random() * 15) + 8);
             },
             select(item) {
-                if(item.status != "ripped"){
+                if(item.status != "ripped" && this.count <= 3){
+                    this.selected.push(item.id);
                     
-                    item.selected = item.selected ? false : true;
-                    if(item.selected){
-                        this.total += item.weight;
-                        this.count++;
-                    }else{
-                        this.total -= item.weight;
-                        this.count--;
-                    }  
-                    console.log(this.total, this.count);
                 }
-                              
-
+                this.selected.forEach(i => {
+                    if(i == item.id){
+                        item.selected = item.selected ? false : true;
+                        if(item.selected){
+                            this.total += item.weight;
+                            this.count++;
+                        }else{
+                            this.total -= item.weight;
+                            this.count--;
+                        }  
+                        
+                        console.log(this.total, this.count);
+                    }
+                })
+                
+                // if(item.status != "ripped"){
+                //     item.selected = item.selected ? false : true;
+                //     if(item.selected){
+                //         this.total += item.weight;
+                //         this.count++;
+                //     }else{
+                //         this.total -= item.weight;
+                //         this.count--;
+                //     }  
+                    
+                //     console.log(this.total, this.count);
+                // }
             }
         }
     }
